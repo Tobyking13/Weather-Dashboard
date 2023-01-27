570799a162fa4756a350a5e183c904fa2081fa63
Toby King <tobyking13@gmail.com>
2023-01-27 12:53:58 +0000

    having issues getting the cityInfo data to show within the cityData function

--- starter/script.js
+++ starter/script.js
@@ -1,55 +1,55 @@
 var weatehrAPIKey = "a38638b883e12f2d3fec49b471358ae8";
 var cityInfo = {
-    cityName: "",
-    cityId: ""
-}
-
-var queryURL = ("https://api.openweathermap.org/data/2.5/weather?id=" + cityInfo.cityId + "&appid=" + weatehrAPIKey);
-
+  cityName: "",
+  cityId: "",
+};
 
 // create function to use search box to get id from city.list.json
 
 // get value from search-input
-// use if state to compare .name to value 
-$("#search-button").click(function(e) {
-    e.preventDefault()
-    getCityId()
-})
+// use if state to compare .name to value
+$("#search-button").click(function (e) {
+  e.preventDefault();
+  getCityId();
+});
 
 function getCityId() {
-    fetch('./starter/city.list.json')
-    .then(function(response) {
-        return response.json();
+  fetch("./starter/city.list.json")
+    .then(function (response) {
+      return response.json();
     })
-    .then(function(data) {
-       
-            var citySearch = $("#search-input").val().toLowerCase();    
-            for(i=0; i<data.length; i++) {
-                var cityList = data[i].name.toLowerCase()
-                if(citySearch === cityList) {
-                    cityInfo.cityName = citySearch;
-                    cityInfo.cityId = data[i].id.toString();
-                }
-            }
-        })
-        
-    cityData(cityInfo);
+    .then(function (data) {
+      var citySearch = $("#search-input").val().toLowerCase();
+      for (i = 0; i < data.length; i++) {
+        var cityList = data[i].name.toLowerCase();
+        if (citySearch === cityList) {
+          cityInfo.cityName = citySearch;
+          cityInfo.cityId = data[i].id.toString();
+        }
+      }
+    });
+
+  cityData(cityInfo);
 }
 
-// getCityId()
 
-function cityData(info) {
+function cityData(cityInfo) {
+  var queryURL =
+    "https://api.openweathermap.org/data/2.5/weather?id=" +
+    cityInfo.cityId +
+    "&appid=" +
+    weatehrAPIKey;
 
-    console.log(this.cityInfo.cityName)
+  console.log(cityInfo.cityName);
 
-
-    $.ajax({
-        url: queryURL,
-        method: 'GET'
-    }).then(function(response) {
-        return console.log(response)
-    }).fail(function(error) {
-        console.log(error);
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+  })
+    .then(function (response) {
+      return console.log(response);
+    })
+    .fail(function (error) {
+      console.log(error);
     });
 }
-
