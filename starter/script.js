580e7557697e3a6cf0184b9ba743e3e93cd1d845
Toby King <tobyking13@gmail.com>
2023-03-09 19:25:31 +0000

    updates code so the search is case insensitive. validity checks for duplicates within saved data & stops user entering places that do not exist

--- starter/script.js
+++ starter/script.js
@@ -1,31 +1,48 @@
-var weatherAPIKey = "a38638b883e12f2d3fec49b471358ae8";
+var weatherAPIKey = "a38638b883e12f2d3fec49b471358ae8"; 
 var cityInfo = {
   cityName: "",
   cityId: "",
   searchHistory: [],
+  cityValid: undefined
 };
 
-$(document).ready(function () {
-  var storedHistory = JSON.parse(localStorage.getItem("search-history"));
-  if (storedHistory !== null) {
-    cityInfo.searchHistory = storedHistory;
-    renderButtons(cityInfo);
+$(document).ready(function () { 
+  var storedHistory = JSON.parse(localStorage.getItem("search-history")); 
+  if (storedHistory !== null) { 
+    cityInfo.searchHistory = storedHistory; 
+    renderButtons(cityInfo); 
   }
 });
 
 $("#search-button").click(function (e) {
-  e.preventDefault();
-  var cityName = $("#search-input").val();
-  cityInfo.searchHistory.push(cityName);
-  cityInfo.cityName = cityName;
-  //getId(cityInfo);
+  e.preventDefault();  
+  //$('#display').text("")
+  var cityName = $("#search-input").val();  
+  let cityNameArr = [];
+  cityName = cityName.toLowerCase().split(' ');
+  for(let i = 0; i < cityName.length; i++) {  
+    cityNameArr.push(cityName[i].charAt(0).toUpperCase() + cityName[i].slice(1));
+  }
+  
+  cityName = cityNameArr.join(' ');
+  cityInfo.cityName = cityName; 
+
+  for(let i = 0; i < cityInfo.searchHistory.length; i++) {
+    if(cityName === cityInfo.searchHistory[i]) {
+      currentForecast(cityInfo);
+      fiveDayForecast(cityInfo);
+      return;
+    }
+  }
+
+  cityInfo.searchHistory.push(cityName); 
   localStorage.setItem(
     "search-history",
     JSON.stringify(cityInfo.searchHistory)
   );
-  renderButtons(cityInfo);
-  cityData(cityInfo);
+  renderButtons(cityInfo); 
 });
+
 
 function renderButtons(cityInfo) {
   $("#history").empty();
@@ -38,7 +55,6 @@ function renderButtons(cityInfo) {
     
       $("#history").append(button);
 
-    
     button.click(function (e) {
       var buttonDataName = e.target.dataset.name;
       cityInfo.cityName = buttonDataName;
@@ -56,67 +72,55 @@ function buttonClick(cityInfo) {
   if(cityInfo.cityName === "") {
     return;
   }
-  cityData(cityInfo)
+  currentForecast(cityInfo);
+  fiveDayForecast(cityInfo);
 }
 
-function cityData(cityInfo) {
-  var id = "";
-  fetch("./starter/city.list.json")
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      for (i = 0; i < data.length; i++) {
-        var cityData = data[i].name;
-        if (cityData === cityInfo.cityName) {
-          // cityInfo.cityId = data[i].id;
-          id = data[i].id;
-        }
-      }
-    })
-    .then(function () {
-      var queryURL =
-        "https://api.openweathermap.org/data/2.5/weather?id=" +
-        id +
-        "&appid=" +
-        weatherAPIKey;
-
-      $.ajax({
-        url: queryURL,
-        method: "GET",
-      }).then(function (response) {
-        var cityName = response.name;
-        var date = moment().format("MMMM Do YYYY");
-        var icon = `https://openweathermap.org/img/wn/${response.weather[0].icon}.png`;
-        var temp = `Temperature: ${(response.main.temp - 273.15).toFixed(0)}°C`;
-        var humidity = `Humidity: ${response.main.humidity}%`;
-        var windSpeed = `Wind speed: ${((response.wind.speed * 3600) / 1000).toFixed(0)} KPH`;
-
-        var iconHeader = $("<img>").attr("src", icon);
-        var cityInfoHeader = $("<h1>")
-          .text(`${cityName}. ${date}`)
-          .append(iconHeader);
-        var tempInfo = $("<p>").text(temp);
-        var windInfo = $("<p>").text(windSpeed);
-        var humidityInfo = $("<p>").text(humidity);
-
-        $("#today").empty();
-        $("#today").attr("class", "border mt-3");
-        $("#today").append(cityInfoHeader);
-        $("#today").append(tempInfo);
-        $("#today").append(windInfo);
-        $("#today").append(humidityInfo);
-      });
-    })
-    .then(function () {
-      fiveDayForcast(id);
-    });
-}
-
-function fiveDayForcast(id) {
+function currentForecast(cityInfo) {
   var queryURL =
-    "https://api.openweathermap.org/data/2.5/forecast?id=" +
-    id +
+  "https://api.openweathermap.org/data/2.5/weather?q=" +
+  cityInfo.cityName +
+  "&appid=" +
+  weatherAPIKey;
+
+$.ajax({
+  url: queryURL,
+  method: "GET",
+}).done(function() {
+  cityInfo.cityValid = true;
+}).fail(function() {
+  cityInfo.cityValid = false;
+  cityValidity(cityInfo);
+})
+.then(function (response) {
+  var cityName = response.name;
+  var date = moment().format("MMMM Do YYYY");
+  var icon = `https://openweathermap.org/img/wn/${response.weather[0].icon}.png`;
+  var temp = `Temperature: ${(response.main.temp - 273.15).toFixed(0)}°C`;
+  var humidity = `Humidity: ${response.main.humidity}%`;
+  var windSpeed = `Wind speed: ${((response.wind.speed * 3600) / 1000).toFixed(0)} KPH`;
+
+  var iconHeader = $("<img>").attr("src", icon);
+  var cityInfoHeader = $("<h1>")
+    .text(`${cityName}. ${date}`)
+    .append(iconHeader);
+  var tempInfo = $("<p>").text(temp);
+  var windInfo = $("<p>").text(windSpeed);
+  var humidityInfo = $("<p>").text(humidity);
+
+  $("#today").empty();
+  $("#today").attr("class", "border mt-3");
+  $("#today").append(cityInfoHeader);
+  $("#today").append(tempInfo);
+  $("#today").append(windInfo);
+  $("#today").append(humidityInfo);
+});
+}
+
+function fiveDayForecast(cityInfo) {
+  var queryURL =
+    "https://api.openweathermap.org/data/2.5/forecast?q=" +
+    cityInfo.cityName +
     "&appid=" +
     weatherAPIKey;
 
@@ -141,7 +145,6 @@ function fiveDayForcast(id) {
       var forecastHeader = $("<h3>").text(date);
       var tempInfo = $("<p>").text(temp);
       var windInfo = $("<p>").text(windSpeed);
-      // windInfo.css('font-size', '100%')
       var humidityInfo = $("<p>").text(humidity);
 
       forecastDiv.append(forecastHeader);
@@ -153,4 +156,13 @@ function fiveDayForcast(id) {
       $("#forecast").append(forecastDiv);
     }
   });
+}
+
+function cityValidity(cityInfo) {
+  if(cityInfo.cityValid === false) {
+    cityInfo.cityName = "";
+    cityInfo.searchHistory.pop();
+    renderButtons(cityInfo);
+    alert(`This place does not exist`)
+  }
 }
